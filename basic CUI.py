a62bbb19ddc483b73848c27da69936fd2f26600d
netagindes <neta.gindes@gmail.com>
2017-06-13 22:00:59 +0300

    Add files via upload
    in case of emergency...

---
+++ basic CUI.py
@@ -0,0 +1,15 @@
+from __future__ import division
+
+import numpy as np
+from scipy import misc, signal
+from matplotlib import pyplot as plt
+
+t_line = input('How long does it take to scan a row? ')
+f_line = 1/t_line
+
+im_name = input('What is the image name? ("copy pathname: /.../file_name.png") ')
+#image name = file_name.png
+
+im = misc.imread(im_name , flatten=True)
+
+l = input('What is the distance between two columns? ([,,,]) ')
